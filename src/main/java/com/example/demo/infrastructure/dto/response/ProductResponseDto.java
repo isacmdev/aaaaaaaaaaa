1c6d9c6e629a19201093ad9e9e86127c71282666
Jonathan Urrego <jadougdev@gmail.com>
2025-11-02 12:57:34 -0500

    funcionando de nuevo

--- src/main/java/com/example/demo/infrastructure/dto/response/ProductResponseDto.java
+++ src/main/java/com/example/demo/infrastructure/dto/response/ProductResponseDto.java
@@ -1,6 +1,5 @@
 package com.example.demo.infrastructure.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
